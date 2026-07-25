7df56d8267e86e3bd34be8846241e2ea46b82b8f
SmallWaterBucket <humanhumanovic@gmail.com>
2026-07-25 17:26:18 +0200

    Changed a char

--- rewrite_to_db.py
+++ rewrite_to_db.py
@@ -2,7 +2,6 @@ import threading,MySQLdb
 password_path = "/home/ubuntu/password.txt"
 def get_db():
     password = open(password_path,"r").read().strip()
-    print(repr(password))
     db = MySQLdb.connect(
         host="localhost",
         user="scratch",
@@ -18,5 +17,5 @@ db = get_db()
 cursor = db.cursor()
 
 for line in file:
-    username, balance = line.strip().split(",")
+    username, balance = line.strip().split("|")
     cursor.execute("INSERT INTO balances (username, balance, read_notifications, accepted) values (%s, %s,0,1);", (username,balance,))
