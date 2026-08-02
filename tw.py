4c13959f884fde7f68a022b0e38c835aab7c50f2
SmallWaterBucket <humanhumanovic@gmail.com>
2026-08-02 18:27:10 +0200

    Cleaned up the code and added comments

--- tw.py
+++ tw.py
@@ -1,19 +1,15 @@
 import scratchattach as sa
-import threading,MySQLdb,time
+import threading ,time
 from datetime import datetime
 from time import strftime
 from random import randint
 from dataclasses import dataclass
 from functools import wraps
 
-path = "/home/ubuntu/password.txt"
-
-
-#session = sa.login_by_id(str(open('sid','r').read()).replace('\n',''), username="SAMURAI228")
-cloud = sa.get_tw_cloud("1365548096")
+cloud = sa.get_tw_cloud("1365548096") # main project that sends all the requests
 client = cloud.requests()
 verificators = {}
-project = sa.get_project("1365548096")
+project = sa.get_project("1366081158") # project to post verification codes in
 
 @dataclass
 class Verificator:
@@ -28,19 +24,18 @@ def ping(): #called when client receives request
 def authenticate(username, prime1, prime2, next_product):
     comments = project.comments(limit=100)
     comment = ""
-    for loop_comment in comments:
+    for loop_comment in comments: # finds the laters comment from a user
         if loop_comment.author_name.lower() == username.lower():
             comment = loop_comment
             break
-
     if comment:
         comment = comment.content
         prime1 = int(prime1)
         prime2 = int(prime2)
-        product = prime1 * prime2
-        if int(product) == int(comment):
+        product = prime1 * prime2 # multiplies primes sent by the user
+        if int(product) == int(comment): # checks the product
             expires_at = int(time.time()) + 10 * 60 #adds ten minures
-            verificators[username] = Verificator(int(next_product), expires_at)
+            verificators[username] = Verificator(int(next_product), expires_at) # adds a verificator item
             print(f"[{get_time()}] Authenticated user {username}")
             return "Authenticated"
     print(f"[{get_time()}] Failed to authenticate user {username}")
@@ -67,7 +62,7 @@ def verify_user(username,prime1,prime2,next_product, function_name):
     prime2 = int(prime2)
     current_product = prime1 * prime2
 
-    if verificator.expires_at < int(time.time()):
+    if verificator.expires_at < int(time.time()): # checks whether the verificator had expired
         verificators.pop(username)
         print(f"[{get_time()}] Verificator for user {username} expired in function: \"{function_name}\"")
         return "Expired"
@@ -80,48 +75,24 @@ def verify_user(username,prime1,prime2,next_product, function_name):
         print(f"[{get_time()}] Failed to verify user: \"{username}\" for function: \"{function_name}\"")
         return "Failed"
 
-@client.request
-@verified_request
-def test(username, text): #username, prime1, prime2, next_product should always be present to be able to authenticate
-    return str(text) + str(username)
-
 
 def get_time():
     return str(datetime.now().strftime("%B %d, %Y  %H:%M:%S"))
-    
 
 
+# Your requests
 @client.request
-def get_balance(username):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT balance from balances where username = %s;",(username,))
-    balance = cursor.fetchall()
-    if not balance or len(balance) == 0:
-        create_user(username)
-    return balance
+@verified_request # include this if you want to check for the legitimacy of the sender of the request
+def test(username, text):  # very simple example for how to use the verified requests
+    return f"{str(username)} : {str(text)}"
 
 
 
-def create_user(username):
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("INSERT INTO balances (username, balance, read_notifications, accepted) values (%s, 100,0,1);", (username,))
-    db.commit()
+
+
 
 @client.event
 def on_ready():
     print("Request handler is running")
-
-
-def get_db():
-    password = open(path,"r").read().strip()
-    db = MySQLdb.connect(
-        host="localhost",
-        user="scratch",
-        passwd=password,
-        database="scratch"
-    )
-    return db
 
 client.start(thread=True) # thread=True is an optional argument. It makes the cloud requests handler run in a thread
